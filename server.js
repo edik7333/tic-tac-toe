bae63a07d495b2b5395cca97c15d90b4db7fa98e
edik sokolov <edik7333@gmail.com>
2023-02-21 11:22:35 +0200

    DDOS protection implementation

--- server.js
+++ server.js
@@ -24,6 +24,16 @@ const path = require("path");
 const app = express();
 const port = 3001;
 
+// set up rate limiter: maximum of five requests per minute
+var RateLimit = require("express-rate-limit");
+var limiter = new RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 5,
+});
+
+// apply rate limiter to all requests
+app.use(limiter);
+
 app.use(express.static(path.join(__dirname, "build")));
 app.use(cookieParser());
 app.use(cors());
